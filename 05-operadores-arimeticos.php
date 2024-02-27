926055cf4f62fde781668ea7ee149b6292f0c12e
leonelmedova <ldavid.mdn@gmail.com>
2024-02-26 19:08:36 -0500

    Cl 05

---
+++ 05-operadores-arimeticos.php
@@ -0,0 +1,43 @@
+<?php
+// Suma
+// $valorA = 5;
+// $valorB = 2;
+
+// $suma = $valorA + $valorB;
+// var_dump($suma);
+
+// Resta
+// $valorA = 5;
+// $valorB = 2;
+
+// $resta = $valorA - $valorB;
+// var_dump($resta);
+
+// Multiplicación
+// $valorA = 5;
+// $valorB = 2;
+
+// $multiplicacion = $valorA * $valorB;
+// var_dump($multiplicacion);
+
+// División
+// $valorA = 5;
+// $valorB = 2;
+
+// $division = $valorA / $valorB;
+// var_dump($division);
+
+// Potenciación
+// $valorA = 5;
+// $valorB = 2;
+
+// $potenciacion = $valorA ** $valorB;
+// var_dump($potenciacion);
+
+// Módulo (Residuo de la división)
+$valorA = 5;
+$valorB = 2;
+
+$modulo = $valorA % $valorB;
+var_dump($modulo);
+?>
