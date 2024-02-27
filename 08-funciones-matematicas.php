d0a5c5f094a2d93489dadc9a8288ffd34a3de0a7
leonelmedova <ldavid.mdn@gmail.com>
2024-02-27 09:15:13 -0500

    Cl 08

---
+++ 08-funciones-matematicas.php
@@ -0,0 +1,23 @@
+<?php
+
+// Funciones con valores máximos y mínimos en un array
+
+// $array = [1, 5, 7, 9, 3];
+
+// echo max($array) . "\n";         // Valor máximo en un array
+
+// echo max(9, 4, 5, 7, 1) . "\n";  // Valor máximo en un array
+// echo min(9, 4, 5, 7, 1) . "\n";  // Valor mínimo en un array
+
+
+// Valores aleatorios dentro de un intervalo
+
+// echo rand(40, 100) . "\n";
+// echo rand() . "\n";
+
+
+// Raíz cuadrada
+
+echo sqrt(25) . "\n";
+
+?>
